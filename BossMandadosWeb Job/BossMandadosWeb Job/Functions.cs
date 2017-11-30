2b46eaaa2f1c35e9847d70a779b0f9f4bb6cc241
Isaac Méndez <isaac95mendez@gmail.com>
2017-11-30 09:29:33 +0900

    Nuevo Web Job

---
+++ BossMandadosWeb Job/BossMandadosWeb Job/Functions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+
+namespace BossMandadosWeb_Job
+{
+    public class Functions
+    {
+        public static void AsignarMandados()
+        {
+
+        }
+    }
+}
